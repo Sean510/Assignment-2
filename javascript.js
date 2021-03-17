9a4e640ee9f346e0933b3ee62f1104cde08a02f5
Sean510 <swhite51096@gmail.com>
2021-03-17 17:59:31 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -6,12 +6,21 @@ document.querySelector("#new-task").onsubmit = function() {
     const li = document.createElement('li');
     
     let task_text = document.querySelector('#task').value;
+    let task_status = document.querySelector('#task-status').value;
     let task_priority = document.querySelector('#task-priority').value;
-    let new_task_html = `
+    if(task-status === 'finished') {
+        let new_task_html = `
+                            <span style="text-decoration:line-through"> ${task_text} </span>
+                            <span style="text-decoration:line-through"> ${task_priority} </span>
+                            <button class="remove"> Remove </button>
+                      `;
+    } else {
+        let new_task_html = `
                             <span> ${task_text} </span>
                             <span> ${task_priority} </span>
                             <button class="remove"> Remove </button>
                       `;
+    }
     li.innerHTML = new_task_html
     
     document.querySelector("#task_list").append(li);
