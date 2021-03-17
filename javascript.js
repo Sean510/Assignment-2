505afa0d6ac0abc934c15004475baa54f7527099
Sean510 <swhite51096@gmail.com>
2021-03-16 21:36:54 -0400

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,27 @@
+//javascript file
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector("#new-task").onsubmit = function() {
+    
+    const li = document.createelement('li');
+    
+    let task_text = document.querySelector('#task').value;
+    let new_task_html = `
+                          <span> ${task_text} </span>
+                          <button class="remove"> Remove </button>
+                        `;
+    li.innerHTML = new_task_html;
+    
+    document.querySelector("#task_list").append(li);
+    document.querySelector("#task").value = '';
+    
+    return false;
+  }
+  
+  document.addEventListener('click', function(event) {
+    element = event.target;
+    if(element.className === 'remove') {
+      element.parentElement.remove();
+    }
+  })
+});
