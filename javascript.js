638aa91077c96c4c7042cad049ca6b3e9e0a0f1e
Sean510 <swhite51096@gmail.com>
2021-03-16 21:46:22 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,14 +3,14 @@
 document.addEventListener('DOMContentLoaded', function() {
 document.querySelector("#new-task").onsubmit = function() {
     
-  const li = document.createelement('li');
+  const li = document.createElement('li');
     
   let task_text = document.querySelector('#task').value;
   let new_task_html = `
                         <span> ${task_text} </span>
                         <button class="remove"> Remove </button>
                       `;
-  li.innerHTML = new_task_html;
+  li.innerHTML = new_task_html
     
   document.querySelector("#task_list").append(li);
   document.querySelector("#task").value = '';
