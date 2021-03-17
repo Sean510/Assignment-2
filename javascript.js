6c69c05289e9cb02542ceee236e35dfb7a72adac
Sean510 <swhite51096@gmail.com>
2021-03-17 18:48:57 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -4,18 +4,19 @@ document.addEventListener('DOMContentLoaded', function() {
 document.querySelector("#new-task").onsubmit = function() {
     
     const li = document.createElement('li');
+    let new_task_html = "";
     
     let task_text = document.querySelector('#task').value;
     let task_status = document.querySelector('#task-status').value;
     let task_priority = document.querySelector('#task-priority').value;
     if(task_status === 'finished') {
-        let new_task_html = `
+         new_task_html = `
                             <span style="text-decoration:line-through"> ${task_text} </span>
                             <span style="text-decoration:line-through"> ${task_priority} </span>
                             <button class="remove"> Remove </button>
                       `;
     } else if(task_status === 'incomplete'){
-        let new_task_html = `
+         new_task_html = `
                             <span> ${task_text} </span>
                             <span> ${task_priority} </span>
                             <button class="remove"> Remove </button>
