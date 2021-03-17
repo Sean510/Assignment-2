d35552131e915fb2f71a4376836c623224842e9f
Sean510 <swhite51096@gmail.com>
2021-03-16 22:47:22 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -6,8 +6,10 @@ document.querySelector("#new-task").onsubmit = function() {
     const li = document.createElement('li');
     
     let task_text = document.querySelector('#task').value;
+    let task_status = document.querySelector('#task-priority').value;
     let new_task_html = `
                             <span> ${task_text} </span>
+                            <span> ${task_status} </span>
                             <button class="remove"> Remove </button>
                       `;
     li.innerHTML = new_task_html
