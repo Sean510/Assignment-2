a10d558f4399905061d7ba35f9cbb04f9777b390
Sean510 <swhite51096@gmail.com>
2021-03-17 19:38:30 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -7,17 +7,18 @@ document.querySelector("#new-task").onsubmit = function() {
     let new_task_html = "";
     
     let task_text = document.querySelector('#task').value;
-    let task_status = document.querySelector('#task-status');
+    let task_completed = document.querySelector('#task-completed');
+    let task_pending = document.querySelector('#task-pending');
     let task_priority = document.querySelector('#task-priority').value;
-    if(task_status.value == 'completed') {
-        task_status.checked = false; 
+    if(task_completed.checked == true) {
+        task_completed.checked = false; 
         new_task_html = `
                             <span style="text-decoration:line-through"> ${task_text} </span>
                             <span style="text-decoration:line-through"> ${task_priority} </span>
                             <button class="remove"> Remove </button>
                       `;
-    } else {
-              task_status.checked = false;
+    } else if(task_pending.checked == true) {
+              task_pending.checked = false;
               new_task_html = `
                             <span> ${task_text} </span>
                             <span> ${task_priority} </span>
