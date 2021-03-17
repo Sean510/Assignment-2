a0eac7560515b212ffc4dce168cc8c36121bbd21
Sean510 <swhite51096@gmail.com>
2021-03-16 21:53:22 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,25 +3,25 @@
 document.addEventListener('DOMContentLoaded', function() {
 document.querySelector("#new-task").onsubmit = function() {
     
-  const li = document.createElement('li');
+    const li = document.createElement('li');
     
-  let task_text = document.querySelector('#task').value;
-  let new_task_html = `
-                        <span> ${task_text} </span>
-                        <button class="remove"> Remove </button>
+    let task_text = document.querySelector('#task').value;
+    let new_task_html = `
+                            <span> ${task_text} </span>
+                            <button class="remove"> Remove </button>
                       `;
-  li.innerHTML = new_task_html
+    li.innerHTML = new_task_html
     
-  document.querySelector("#task_list").append(li);
-  document.querySelector("#task").value = '';
+    document.querySelector("#task_list").append(li);
+    document.querySelector("#task").value = '';
     
-  return false;
-  }
+    return false;
+}
   
   document.addEventListener('click', function(event) {
     element = event.target;
     if(element.className === 'remove') {
       element.parentElement.remove();
     }
-  })
+ })
 });
