1edd0f6f6e2a5534be58ea4ea656ccd09c10e8cf
Sean510 <swhite51096@gmail.com>
2021-03-17 21:49:28 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -44,7 +44,7 @@ document.querySelector("#new-task").onsubmit = function() {
     }
       
     if(element.className === 'mark') {
-      li.innerHTML = "abc";
+      element.parentElement.strike();
     }
  })
 });
